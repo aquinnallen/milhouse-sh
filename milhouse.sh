a964f16ee2a0a9a2bcd4ffba15963dac5a658d58
Ubuntu <ubuntu@ip-172-31-33-224.us-east-2.compute.internal>
2024-01-19 20:12:12 +0000

    improved gui

--- milhouse.sh
+++ milhouse.sh
@@ -3,7 +3,7 @@ sek=0
 echo "Milhouses incoming!"
 while true
 do
-   echo -ne "$sek Milhouses have occured... \r"
+   echo -ne "---> $sek Milhouses have occured... \r"
    sleep 1
    sek=$[$sek+1]
 done
